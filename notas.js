d4918c6ff15b7dd3294ad9480b3940c0c89a5388
Dalia Pastene <129213450+DaliaMultimedia@users.noreply.github.com>
2025-11-15 10:50:18 -0300

    Add files via upload

---
+++ notas.js
@@ -0,0 +1,11 @@
+//Pantalla de inicio ANDA
+//Al apretar enter inicia el juego ANDA
+//Al apretar W salta ANDA
+//Al saltar esquiva los hoyos ANDA
+
+
+//Al tocar el hoyo aparece la pantalla de perder ANDA
+//Al pasar 30 segundos aparece la pantalla de ganar ANDA
+//Se puede reiniciar el juego con R en ambas pantallas ANDA
+
+//El Juego completo funciona
